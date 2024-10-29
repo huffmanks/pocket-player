751e42cf5d6f4c8a735f695e4eb0ac11cbc67f42
Kevin Huffman <huffmanks2@gmail.com>
2024-10-29 16:46:48 -0400

    reinit db

--- db/migrations/0000_bitter_agent_brand.sql
+++ db/migrations/0000_bitter_agent_brand.sql
@@ -2,5 +2,6 @@ CREATE TABLE `videos` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text DEFAULT 'Untitled' NOT NULL,
 	`fileUri` text NOT NULL,
-	`created_at` text DEFAULT '2024-10-28T01:45:17.395Z'
+	`isFavorite` integer DEFAULT false,
+	`created_at` text DEFAULT '2024-10-29T20:46:29.154Z'
 );
