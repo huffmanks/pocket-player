ebee759231dd5867242a45996760c8decf8908d8
Kevin Huffman <55319887+huffmanks@users.noreply.github.com>
2025-04-19 17:07:20 -0400

    Update android-navigation-bar.ts

--- lib/android-navigation-bar.ts
+++ lib/android-navigation-bar.ts
@@ -6,8 +6,8 @@ import { NAV_THEME } from "@/lib/constants";
 export async function setAndroidNavigationBar(theme: "light" | "dark") {
   if (Platform.OS !== "android") return;
 
-  await setButtonStyleAsync(theme === "dark" ? "light" : "dark");
+  await setButtonStyleAsync(theme === "light" ? "dark" : "light");
   await setBackgroundColorAsync(
-    theme === "dark" ? NAV_THEME.dark.background : NAV_THEME.light.background
+    theme === "light" ? NAV_THEME.light.background : NAV_THEME.dark.background
   );
 }
