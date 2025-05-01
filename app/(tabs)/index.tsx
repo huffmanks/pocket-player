42b126c778b5709a9e030b5fab948b9089c83280
Kevin Huffman <huffmanks2@gmail.com>
2025-05-01 15:13:27 -0400

    init splash screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,38 @@
+import { Link } from "expo-router";
 import { Image, View } from "react-native";
+
+import { CloudUploadIcon } from "@/lib/icons";
+
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { H1 } from "@/components/ui/typography";
 
 export default function IndexScreen() {
   return (
     <View className="flex-1 items-center justify-center">
       <Image
+        className="mb-10"
         style={{ width: 75, height: 75 }}
         source={require("../../assets/icons/base_logo.png")}
       />
+      <H1 className="mb-3 text-foreground">Pocket Player</H1>
+      <Text className="mb-10 text-muted-foreground">Get started by uploading some videos.</Text>
+      <Link
+        href="/(tabs)/upload"
+        asChild>
+        <Button
+          size="lg"
+          className="flex flex-row items-center justify-center gap-4 bg-brand">
+          <CloudUploadIcon
+            className="text-foreground"
+            size={24}
+            strokeWidth={1.5}
+          />
+          <Text className="native:text-lg font-semibold uppercase tracking-wider text-foreground">
+            Upload
+          </Text>
+        </Button>
+      </Link>
     </View>
   );
 }
